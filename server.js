f8356bf60d9039da6f6017e6ebf0854d08f8cb9c
Albert <albertmabo001@gmail.com>
2025-08-15 12:06:43 +0545

    Setup Prod and Dev env

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-const dotenv = rquire("")
+const dotenv = require("dotenv");
+
+// Loading environment sepcific config
+
+if(process.env.NODE_ENV === 'production'){
+    dotenv.config({path:'.env.production'});
+}else{
+    dotenv.config({path:'.env.development'});
+}
+
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 3001;
+
+const start = () =>{
+    try {
+        app.listen(PORT, ()=>{
+            console.log(`Server is running at PORT ${PORT}`);
+            console.log(`Running on ${process.env.NODE_ENV} mode`);
+            
+            
+        })
+    } catch (error) {
+        console.error(errror);
+        
+    }
+}
+
+start();
